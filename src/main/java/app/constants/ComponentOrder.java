e7d798ea69c529b61bc4d4d99f6bfba76f406b52
r-fujiyama <hujiyama2843@gmail.com>
2023-04-06 22:58:35 +0900

    Fix to refer to Ordered

--- src/main/java/app/constants/ComponentOrder.java
+++ src/main/java/app/constants/ComponentOrder.java
@@ -1,8 +1,9 @@
 package app.constants;
 
+import org.springframework.core.Ordered;
+
 public class ComponentOrder {
 
-  private static final int HIGHEST_PRECEDENCE = Integer.MIN_VALUE;
-  public static final int LOGGING_FILTER = HIGHEST_PRECEDENCE;
+  public static final int LOGGING_FILTER = Ordered.HIGHEST_PRECEDENCE;
 
 }
