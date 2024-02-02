bd5eb6b5c996e73551bc71b959fb76d98438722a
r-fujiyama <hujiyama2843@gmail.com>
2024-02-02 15:39:27 +0900

    Fix echo command

--- docker/run-db.ps1
+++ docker/run-db.ps1
@@ -5,5 +5,4 @@ cd $targetFolder
 wsl.exe -d Ubuntu-22.04 docker compose down
 wsl.exe -d Ubuntu-22.04 docker compose build
 wsl.exe -d Ubuntu-22.04 docker compose up
-echo "Container startup is complete."
 pause
