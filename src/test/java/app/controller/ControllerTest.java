247762368210f34a281a967e772c8b31ce599435
r-fujiyama <hujiyama2843@gmail.com>
2024-02-09 00:00:26 +0900

    Fix test

--- src/test/java/app/controller/ControllerTest.java
+++ src/test/java/app/controller/ControllerTest.java
@@ -1,15 +1,19 @@
 package app.controller;
 
 import app.config.properties.SecurityProperties;
+import app.dao.UserDao;
 import app.util.MessageUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+@ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Import({SecurityProperties.class, MessageUtils.class})
 public abstract class ControllerTest {
@@ -17,6 +21,8 @@ public abstract class ControllerTest {
   @Autowired
   private WebApplicationContext webApplicationContext;
   protected MockMvc mockMvc;
+  @MockBean
+  UserDao userDao;
 
   @BeforeAll
   public void setupMockMvc() {
