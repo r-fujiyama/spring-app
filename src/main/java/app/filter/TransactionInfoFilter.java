ad7aa0eac0ebf3970d29d3ee5fd1b1718a42b774
r-fujiyama <hujiyama2843@gmail.com>
2024-02-13 14:03:09 +0900

    Fix to not initialize TransactionInfo in case of anonymous authentication

--- src/main/java/app/filter/TransactionInfoFilter.java
+++ src/main/java/app/filter/TransactionInfoFilter.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import lombok.AllArgsConstructor;
 import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -25,10 +26,12 @@ public class TransactionInfoFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
       @NonNull FilterChain filterChain) throws ServletException, IOException {
-    var userID = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    var user = UserDao.findByUserID(userID);
-    if (user != null) {
-      TransactionInfo.init(user.getUserType());
+    if (!(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)) {
+      var userID = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      var user = UserDao.findByUserID(userID);
+      if (user != null) {
+        TransactionInfo.init(user.getUserType());
+      }
     }
     filterChain.doFilter(request, response);
   }
