e4b996a72a3469dcf33dbaa58885b43e4f9b7a7f
r-fujiyama <hujiyama2843@gmail.com>
2024-02-14 13:01:55 +0900

    Fix uri

--- src/main/java/app/controller/health/HealthCheckController.java
+++ src/main/java/app/controller/health/HealthCheckController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-  @GetMapping("v1/health-check")
-  public Response v1HealthCheck() {
+  @GetMapping("v1/health")
+  public Response v1Health() {
     return new Response();
   }
 
-  @GetMapping("v2/health-check")
-  public Response v2HealthCheck() {
+  @GetMapping("v2/health")
+  public Response v2Health() {
     return new Response();
   }
 
