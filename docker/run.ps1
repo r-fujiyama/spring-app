82811461384ac54a2079834b0e07c40ee4cdf034
r-fujiyama <hujiyama2843@gmail.com>
2024-03-08 17:41:17 +0900

    Fix space

--- docker/run.ps1
+++ docker/run.ps1
@@ -5,7 +5,7 @@ if ($status.Split(' ')[4] -eq 'not') {
 
 $userName = $env:UserName
 $targetFolder = "\\wsl$\Ubuntu-22.04\home\${userName}\develop\spring-app\docker"
-Copy-Item ".\" -Recurse $targetFolder -Exclude *.ps1  -Force
+Copy-Item ".\" -Recurse $targetFolder -Exclude *.ps1 -Force
 cd $targetFolder
 wsl.exe -d Ubuntu-22.04 docker compose down
 wsl.exe -d Ubuntu-22.04 docker compose build
