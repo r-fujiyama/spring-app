c92d4dcbdf7bd7e0aee4019f6f680ef7240f0325
r-fujiyama <hujiyama2843@gmail.com>
2024-03-10 02:07:56 +0900

    Add InsertUserTest

--- src/test/java/app/service/ServiceTest.java
+++ src/test/java/app/service/ServiceTest.java
@@ -1,17 +1,22 @@
 package app.service;
 
 import app.util.MessageUtils;
-import com.github.database.rider.spring.api.DBRider;
+import com.github.database.rider.junit5.api.DBRider;
+import org.junit.jupiter.api.TestInstance;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
 @MybatisTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @DBRider
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Import({MessageUtils.class})
 public abstract class ServiceTest {
 
---
+++ src/test/java/app/service/userV1/FreelanceUserV1ServiceTest/InsertUserTest.java
@@ -0,0 +1,52 @@
+package app.service.userV1.FreelanceUserV1ServiceTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import app.TransactionInfo;
+import app.enums.UserStatus;
+import app.enums.UserType;
+import app.service.ServiceTest;
+import app.service.userV1.FreelanceUserV1Service;
+import app.service.userV1.parameter.InsertUserParam;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+@Import(FreelanceUserV1Service.class)
+public class InsertUserTest extends ServiceTest {
+
+  private final static String TEST_DATA_BASE_PATH = "test-data/service/userV1/FreelanceUserV1ServiceTest/InsertUserTest/";
+
+  @Autowired
+  private FreelanceUserV1Service service;
+
+  @BeforeAll
+  public void beforeAll() {
+    TransactionInfo.init("test_user", UserType.FREELANCE);
+  }
+
+  @Test
+  @DataSet(TEST_DATA_BASE_PATH + "clean-user.yml")
+  @ExpectedDataSet(TEST_DATA_BASE_PATH + "expected-user.yml")
+  public void insertUserTest() {
+    var actual = service.insertUser(InsertUserParam.builder()
+        .userName("test_user_1")
+        .password("password")
+        .userType(UserType.FREELANCE)
+        .firstName("ichiro")
+        .lastName("tokyo")
+        .age(20)
+        .build());
+    assertThat(actual.getId()).isNotZero();
+    assertThat(actual.getName()).isEqualTo("test_user_1");
+    assertThat(actual.getType()).isEqualTo(UserType.FREELANCE);
+    assertThat(actual.getStatus()).isEqualTo(UserStatus.REGISTERED);
+    assertThat(actual.getFirstName()).isEqualTo("ichiro");
+    assertThat(actual.getLastName()).isEqualTo("tokyo");
+    assertThat(actual.getAge()).isEqualTo(20);
+  }
+
+}
