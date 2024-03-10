62a69515f2195a398302c1a6bcd6f61a54842a23
r-fujiyama <hujiyama2843@gmail.com>
2024-03-10 18:35:19 +0900

    Add insert user test case

--- src/test/java/app/service/ServiceTest.java
+++ src/test/java/app/service/ServiceTest.java
@@ -4,6 +4,8 @@ import app.util.MessageUtils;
 import com.github.database.rider.junit5.api.DBRider;
 import org.junit.jupiter.api.TestInstance;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.context.annotation.Import;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(propagation = Propagation.NOT_SUPPORTED)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Import({MessageUtils.class})
+@ImportAutoConfiguration(MessageSourceAutoConfiguration.class)
 public abstract class ServiceTest {
 
   protected static final String CLEAN_TABLE_DATA_BASE_PATH = "test-data/clean-table/";
--- src/test/java/app/service/userV1/FreelanceUserV1ServiceTest/InsertUserTest.java
+++ src/test/java/app/service/userV1/FreelanceUserV1ServiceTest/InsertUserTest.java
@@ -1,10 +1,13 @@
 package app.service.userV1.FreelanceUserV1ServiceTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import app.TransactionInfo;
+import app.enums.ErrorCode;
 import app.enums.UserStatus;
 import app.enums.UserType;
+import app.exception.ConflictException;
 import app.service.ServiceTest;
 import app.service.userV1.FreelanceUserV1Service;
 import app.service.userV1.parameter.InsertUserParam;
@@ -14,6 +17,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 
 @Import(FreelanceUserV1Service.class)
 public class InsertUserTest extends ServiceTest {
@@ -47,6 +51,29 @@ public class InsertUserTest extends ServiceTest {
     assertThat(actual.getFirstName()).isEqualTo("ichiro");
     assertThat(actual.getLastName()).isEqualTo("tokyo");
     assertThat(actual.getAge()).isEqualTo(20);
+  }
+
+  @Test
+  @DataSet(TEST_DATA_BASE_PATH + "setup-user.yml")
+  @ExpectedDataSet(TEST_DATA_BASE_PATH + "setup-user.yml")
+  public void existsUserInsertTest() {
+    assertThatThrownBy(() -> service.insertUser(InsertUserParam.builder()
+        .userName("test_user_1")
+        .password("password")
+        .userType(UserType.FREELANCE)
+        .firstName("ichiro")
+        .lastName("tokyo")
+        .age(20)
+        .build())
+    ).isInstanceOfSatisfying(ConflictException.class, e -> {
+      assertThat(e.getMessage()).isEqualTo(
+          "ErrorCode:Conflict, Message:ユーザー:'test_user_1'は既に存在します。追加することは出来ません。");
+      assertThat(e.getHttpStatus()).isEqualTo(HttpStatus.CONFLICT);
+      assertThat(e.getErrors()).hasSize(1);
+      assertThat(e.getErrors().getFirst().getCode()).isEqualTo(ErrorCode.CONFLICT);
+      assertThat(e.getErrors().getFirst().getMessage()).isEqualTo(
+          "ユーザー:'test_user_1'は既に存在します。追加することは出来ません。");
+    });
   }
 
 }
